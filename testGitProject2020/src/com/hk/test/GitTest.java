6d3a0607d8be104709231bc03e931bf8ce882901
user <user@DESKTOP-AUU34GB>
2020-07-17 10:31:40 +0900

    ㅁㅇㅁ

--- testGitProject2020/src/com/hk/test/GitTest.java
+++ testGitProject2020/src/com/hk/test/GitTest.java
@@ -5,7 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("새로운 내용 추가");
 		System.out.println("kang1이 추가");
-
+		System.out.println("kang2가 추가");
 	}
 
 }
